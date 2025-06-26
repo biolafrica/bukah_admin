d818ad5940485b7744620578fbc9c4253fd4cd95
Abiodun Biobaku <biolafrica@gmail.com>
2025-06-26 02:44:02 +0100

    mono repo setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "apps/**/*/pages/**/*.{js,jsx,ts,tsx}",
+    "apps/**/*/components/**/*.{js,jsx,ts,tsx}",
+    "apps/mobile-ops-app/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
